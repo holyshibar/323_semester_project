37ea285dec77a3ec0cff1623ee0586149583087f
WebForks <35718739+WebForks@users.noreply.github.com>
2024-03-08 20:39:54 -0800

    polish up readme and cleared up files

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 We make a .exe and when the user clicks to run it, it brings up a menu that allows the user point at the game that the user wants to decrypt/disconnect from steam. After that it checks what drm the game uses and if it uses a steam drm we decrypt it using ([https://github.com/atom0s/Steamless](https://github.com/atom0s/Steamless "https://github.com/atom0s/Steamless")) and then modify some files and emulate it using ([https://gitlab.com/Mr_Goldberg/goldberg_emulator](https://gitlab.com/Mr_Goldberg/goldberg_emulator "https://gitlab.com/Mr_Goldberg/goldberg_emulator")).
 
-- /make .exe and gui that allows user to point at game folder user wants to decrypt/disconnect from steam
-- /integrate or use pcgamerwiki api to look at what drm the game has.
-- if game has steam drm use steamless cli to decrypt
-- If game doesnt have drm just emulate
-- if game has other drm other then those then report back can not emulate or decrypt.
+(almost done. Need to redirect stdout to the log area of the gui)make .exe and gui that allows user to point at game folder user wants to decrypt/disconnect from steam
 
-work on:
+(done) integrate or use pcgamerwiki api to look at what drm the game has.
 
-https://www.reddit.com/r/learnpython/comments/io33de/redirect_stdout_to_tkinter_text_widget/
+X if game has steam drm use steamless cli to decrypt
+
+X If game doesnt have drm just emulate
+
+X if game has other drm other then those then report back can not emulate or decrypt.
+
+**Usage:**
+
+pip install requirements.txt
+
+to build: pyinstaller --onefile --windowed main.py (.exe in dist folder)
--- main.py
+++ main.py
@@ -1,39 +1,55 @@
-# rm -r .\build\, .\dist\, .\main.spec
-# pyinstaller --onefile --windowed main.py
-
 import tkinter as tk
 from tkinter import filedialog, scrolledtext
+from drm_analysis import DRMAnalysis  # Ensure to import the DRMAnalysis class
+import os
 
 
 def browse_folder():
     folder_path = filedialog.askdirectory()
-    path_label.config(text=folder_path)
+    if folder_path:  # Check if a folder was actually selected
+        # Extracts the name of the game from the folder path
+        game_name = os.path.basename(folder_path)
+        # Set the extracted game name as the label text
+        path_label.config(text=game_name)
+    else:
+        path_label.config(text="No folder selected")
 
 
 def decrypt():
-    # Example logs
-    logs = ["This is a test", "loading....."]
-    for log in logs:
-        log_area.insert(tk.END, log + "\n")
-    log_area.yview(tk.END)  # Auto-scroll to the end
+    # This now gets the extracted game name
+    game_name = path_label.cget("text")
+    if game_name == "No folder selected":
+        log_area.insert(tk.END, "Please select a folder first.\n")
+        return
+
+    drm_analysis = DRMAnalysis(game_name)
+    availability_section, denuvo_detected = drm_analysis.get_pcgamingwiki_info()
+
+    if denuvo_detected:
+        log_area.insert(tk.END, "Denuvo Anti-Tamper detected.\n")
+    elif availability_section:
+        analysis_result = drm_analysis.analyze_steam_availability(
+            availability_section)
+        log_area.insert(tk.END, analysis_result)
+    else:
+        log_area.insert(
+            tk.END, "Availability section not found or failed to retrieve data.\n")
+
+    log_area.yview(tk.END)
 
 
 app = tk.Tk()
 app.title('Game Folder Selector')
 
-# Browse button
 browse_button = tk.Button(app, text="Browse", command=browse_folder)
 browse_button.pack()
 
-# Path label
 path_label = tk.Label(app, text="No folder selected")
 path_label.pack()
 
-# Log area
 log_area = scrolledtext.ScrolledText(app, width=40, height=10, state='normal')
 log_area.pack(pady=10)
 
-# Decrypt button
 decrypt_button = tk.Button(app, text="Decrypt", command=decrypt)
 decrypt_button.pack()
 
