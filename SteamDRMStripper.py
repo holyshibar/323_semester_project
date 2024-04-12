4db5c0e18764dafd58989733d5714baa556064a8
cdv0 <155689506+cdv0@users.noreply.github.com>
2024-04-11 20:58:01 -0700

    Add print statement

--- SteamDRMStripper.py
+++ SteamDRMStripper.py
@@ -16,6 +16,7 @@ def unpack_with_steamless(game_exe_path, steamless_path):
             run_steamless = subprocess.run([steamless_cli_path, game_exe_path], capture_output=True, text=True)
             if run_steamless.returncode != 0:
                 print("Error:", run_steamless.stderr) #Error message may be empty
+                print("Could not unpack file.")
             else:
                 print("Successfully unpacked file.")
                 unpacked_file_path = find_unpacked_file(game_exe_path)
